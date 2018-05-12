0b7d59be4027523ad03381173166685344a719c1
alexrribeiro <alexramosribeiro@gmail.com>
2018-05-12 09:53:16 -0300

    Implementação

--- src/main/java/br/unisal/service/CalculatorService.java
+++ src/main/java/br/unisal/service/CalculatorService.java
@@ -48,4 +48,22 @@ public class CalculatorService implements Calculable {
 		return dividend / divisor;
 	}
 
+	@Override
+	public double sum(double portion1, double portion2) {
+		// TODO Auto-generated method stub
+		return portion1 + portion2;
+	}
+
+	@Override
+	public double subtract(double minuend, double subtrahend) {
+		// TODO Auto-generated method stub
+		return minuend - subtrahend;
+	}
+
+	@Override
+	public double multiply(double multiplicand, double multiplier) {
+		// TODO Auto-generated method stub
+		return multiplicand * multiplier;
+	}
+
 }
