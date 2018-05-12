2db4d25e8b305be0538d17f09db3b1839a8dfafa
alexrribeiro <alexramosribeiro@gmail.com>
2018-05-12 09:08:02 -0300

    First Version

--- src/main/java/br/unisal/interfaces/Calculable.java
+++ src/main/java/br/unisal/interfaces/Calculable.java
@@ -21,9 +21,58 @@ import static br.unisal.util.Constants.*;
 @SOAPBinding(style = Style.DOCUMENT) 
 public interface Calculable {
 	
+	/**
+	 * 
+	 * @param dividend
+	 * @param divisor
+	 * @return
+	 * @throws DivisionByZeroArithymeticException
+	 * @throws SOAPException
+	 */
+	
 	@WebMethod(action=HOST + "/divide", operationName="division")
 	public double divide(
 		@WebParam(name = "dividend") double dividend,
 		@WebParam(name = "divisor") double divisor
 	) throws DivisionByZeroArithymeticException, SOAPException;
+	
+	
+	/**
+	 * 
+	 * @param portion1
+	 * @param portion2
+	 * @return
+	 */
+	
+	@WebMethod(action=HOST + "/sum", operationName="sum")
+	public double sum (
+		@WebParam(name = "portion1") double portion1,
+		@WebParam(name = "portion2") double portion2
+	);
+	
+	/**
+	 * 
+	 * @param minuend
+	 * @param subtrahend
+	 * @return
+	 */
+	
+	@WebMethod(action=HOST + "/subtract", operationName="difference")
+	public double subtract (
+		@WebParam(name = "minuend") double minuend,
+		@WebParam(name = "subtrahend") double subtrahend
+	);
+	
+	/**
+	 * 
+	 * @param multiplicand
+	 * @param multiplier
+	 * @return
+	 */
+	
+	@WebMethod(action=HOST + "/multiply", operationName="product")
+	public double multiply (
+		@WebParam(name = "multiplicand") double multiplicand,
+		@WebParam(name = "multiplier") double multiplier
+	);
 }
